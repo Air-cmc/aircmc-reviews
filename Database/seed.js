aa40012679a7e4d7a558019986d32e3d2a09e4e0
Your Name <mikeetree@gmail.com>
2021-02-06 00:13:22 -0500

    Add timings

--- Database/seed.js
+++ Database/seed.js
@@ -41,8 +41,7 @@ const ratingsReadStream = new GeneratorStream(recordsCount, (counter) => {
   return s;
 });
 
-
-const reviewsWriteStream = fs.createWriteStream('reviews.csv');
+let reviewsCount = 0;
 
 const reviewsReadStream = new GeneratorStream(recordsCount, (counter) => {
   let s = '';
@@ -54,10 +53,49 @@ const reviewsReadStream = new GeneratorStream(recordsCount, (counter) => {
 
   for (let i = 0; i < rand; i++) {
     s += `${counter},` + rowFromObject(generator.review());
+    reviewsCount++;
   }
 
   return s;
 });
 
-ratingsReadStream.pipe(fs.createWriteStream(path.resolve(__dirname, './ratings.csv')));
-reviewsReadStream.pipe(fs.createWriteStream(path.resolve(__dirname, './reviews.csv')));
+writeRatings = () => {
+  return new Promise((resolve, reject) => {
+    const writeStream = fs.createWriteStream(path.resolve(__dirname, './ratings.csv'));
+
+    ratingsReadStream.pipe(writeStream);
+
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
+  });
+};
+
+writeReviews = () => {
+  return new Promise((resolve, reject) => {
+    const writeStream = fs.createWriteStream(path.resolve(__dirname, './reviews.csv'));
+
+    reviewsReadStream.pipe(writeStream);
+
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
+  });
+};
+
+
+(async function writeAll () {
+  console.info(`Generating ${recordsCount.toLocaleString()} primary records...`);
+
+  let tstart = process.hrtime();
+  await writeRatings();
+  let tend = process.hrtime(tstart);
+
+  console.info(`Generated ${recordsCount.toLocaleString()} primary records in  %ds %dms`, tend[0], tend[1] / 1000000);
+
+  console.info(`Generating secondary records...`);
+
+  tstart = process.hrtime();
+  await writeReviews();
+  tend = process.hrtime(tstart);
+
+  console.info(`Generated ${reviewsCount.toLocaleString()} secondary records in  %ds %dms`, tend[0], tend[1] / 1000000);
+})();
