c1c073e30ba20faf03a02aa0e258281f37fd1be8
Your Name <mikeetree@gmail.com>
2021-02-08 21:51:14 -0500

    Update API methods

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -17,70 +18,69 @@ app.use(express.static(path.join(__dirname + '/../client/dist')));
 
 app.get('/review/:id', async (req, res) => {
   const { id } = req.params;
-  let result;
 
   try {
-    result = await db.query('SELECT * FROM reviews WHERE listing_id = $1', [id]);
+    const result = await db.query('SELECT * FROM reviews WHERE listing_id = $1', [id]);
+
+    if (result.rowCount > 0) {
+      const reviews = [];
+
+      for (review of result.rows) {
+        reviews.push({review});
+      }
+
+      res.status(200);
+      res.send(reviews);
+    } else {
+      res.status(200).end();
+    }
   } catch (err) {
     console.log(err);
-    res.end();
-  }
-
-  if (result.rowCount > 0) {
-    const reviews = [];
-
-    for (review of result.rows) {
-      reviews.push({review});
-    }
-
-    res.send(reviews);
-  } else {
-    res.end();
+    res.status(500).end();
   }
 });
 
 app.get('/rating/:id', async (req, res) => {
   const { id } = req.params;
-  let result;
 
   try {
-    result = await db.query('SELECT * FROM ratings WHERE listing_id = $1', [id]);
+    const result = await db.query('SELECT * FROM ratings WHERE listing_id = $1', [id]);
+
+    if (result.rowCount > 0) {
+      const r = result.rows[0];
+      const rating = {
+        starReview: {
+          1: r['1_stars'],
+          2: r['2_stars'],
+          3: r['3_stars'],
+          4: r['4_stars'],
+          5: r['5_stars'],
+        },
+        starOptions: {
+          Cleanliness: r.cleanliness,
+          Communication: r.communication,
+          CheckIn: r.checkin,
+          Accuracy: r.accuracy,
+          Location: r.location,
+          Value: r.value,
+        },
+        options: {
+          comfortableBeds: r.comfortable_beds,
+          responsiveHost: r.responsive_host,
+          greatLocation: r.great_location,
+          greatViews: r.great_views,
+          easyCheckIn: r.easy_checkin,
+          greatRestaurants: r.great_restaurants,
+          centralLocation: r.central_location,
+        },
+      }
+      res.status(200).send(rating);
+    } else {
+      res.status(200).end();
+    }
   } catch (err) {
     console.log(err);
-    res.end();
-  }
-
-  if (result.rowCount > 0) {
-    const r = result.rows[0];
-    const rating = {
-      starReview: {
-        1: r['1_stars'],
-        2: r['2_stars'],
-        3: r['3_stars'],
-        4: r['4_stars'],
-        5: r['5_stars'],
-      },
-      starOptions: {
-        Cleanliness: r.cleanliness,
-        Communication: r.communication,
-        CheckIn: r.checkin,
-        Accuracy: r.accuracy,
-        Location: r.location,
-        Value: r.value,
-      },
-      options: {
-        comfortableBeds: r.comfortable_beds,
-        responsiveHost: r.responsive_host,
-        greatLocation: r.great_location,
-        greatViews: r.great_views,
-        easyCheckIn: r.easy_checkin,
-        greatRestaurants: r.great_restaurants,
-        centralLocation: r.central_location,
-      },
-    }
-    res.send(rating);
-  } else {
-    res.end();
+    res.status(500).end();
   }
 });
 
